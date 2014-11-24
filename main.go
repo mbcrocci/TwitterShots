7701970c0c5c968aadfd260f98967cdf5805ed10
Maurizio Crocci <mbcrocci@gmail.com>
2014-11-24 13:56:06 +0000

    mongodb added

--- main.go
+++ main.go
@@ -1,28 +1,64 @@
 package main
 
 import (
-	"io/ioutil"
+	"fmt"
+	"os"
 
+	"labix.org/v2/mgo"
+
+	"github.com/codegangsta/martini-contrib/binding"
+	"github.com/codegangsta/martini-contrib/render"
 	"github.com/go-martini/martini"
 )
 
+type SearchWord struct {
+	Word string `form:"word"`
+}
+
+func DB() martini.Handler {
+	session, err := mgo.Dial(os.Getenv("MONGO_URL"))
+	if err != nil {
+		panic(err)
+	}
+
+	return func(c martini.Context) {
+		s := session.Clone()
+		c.Map(s.DB(os.Getenv("MONGO_DB")))
+		defer s.Close()
+		c.Next()
+	}
+}
+
+func GetAll(db *mgo.Database) []SearchWord {
+	var wordlist []SearchWord
+	db.C("words").Find(nil).All(&wordlist)
+	return wordlist
+}
+
 func main() {
+
+	if connectToMongo() {
+		fmt.Println("Connected")
+	} else {
+		fmt.Println("Not Connected")
+	}
+
 	m := martini.Classic()
-	page := loadPage("index")
+	m.Use(render.Renderer())
+	m.Use(DB())
 
-	m.Get("/", func() string {
-		return string(page.Body)
+	m.Get("/", func(r render.Render, db *mgo.Database) {
+		r.HTML(200, "index", GetAll(db))
 	})
+
+	m.Post("/", binding.Form(SearchWord{}), func(word SearchWord, r render.Render, db *mgo.Database) {
+		db.C("words").Insert(word)
+		r.HTML(200, "index", GetAll(db))
+	})
+
+	m.NotFound(func() string {
+		return "Someone stole this page :o"
+	})
+
 	m.Run()
-}
-
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func loadPage(title string) *Page {
-	filename := title + ".html"
-	body, _ := ioutil.ReadFile(filename)
-	return &Page{Title: title, Body: body}
 }
